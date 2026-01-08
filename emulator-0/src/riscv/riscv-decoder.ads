8234a384c8c44cc9e02c25b28906302088a09d13
Rongcui Dong <rongcuid@outlook.com>
2026-01-08 20:09:47 +0000

    Minor fixes

--- emulator-0/src/riscv/riscv-decoder.ads
+++ emulator-0/src/riscv/riscv-decoder.ads
@@ -1,4 +1,5 @@
 with Interfaces; use Interfaces;
+with System;
 
 package RISCV.Decoder is
    type Opcode_Field is mod 2**7;
@@ -8,14 +9,14 @@ package RISCV.Decoder is
    type Imm_11_0_Field is mod 2**12;
    type Imm_4_0_Field is mod 2**5;
    type Imm_11_5_Field is mod 2**7;
-   type Imm_11_Field is mod 2;
+   type Imm_11_Field is mod 2**1;
    type Imm_4_1_Field is mod 2**4;
    type Imm_10_5_Field is mod 2**6;
-   type Imm_12_Field is mod 2;
+   type Imm_12_Field is mod 2**1;
    type Imm_31_12_Field is mod 2**20;
    type Imm_19_12_Field is mod 2**8;
    type Imm_10_1_Field is mod 2**10;
-   type Imm_20_Field is mod 2;
+   type Imm_20_Field is mod 2**1;
 
    type R_Type is record
       Opcode : Opcode_Field;
@@ -35,6 +36,7 @@ package RISCV.Decoder is
        R_S2 at 0 range 20 .. 24;
        Func_7 at 0 range 25 .. 31;
      end record;
+   for R_Type'Bit_Order use System.Low_Order_First;
 
    type I_Type is record
       Opcode   : Opcode_Field;
@@ -52,10 +54,11 @@ package RISCV.Decoder is
        R_S1 at 0 range 15 .. 19;
        Imm_11_0 at 0 range 20 .. 31;
      end record;
+   for I_Type'Bit_Order use System.Low_Order_First;
 
    type S_Type is record
       Opcode   : Opcode_Field;
-      Imm_4_0  : Imm_4_0_field;
+      Imm_4_0  : Imm_4_0_Field;
       Func_3   : Func_3_Field;
       R_S1     : Register_Field;
       R_S2     : Register_Field;
@@ -71,6 +74,7 @@ package RISCV.Decoder is
        R_S2 at 0 range 20 .. 24;
        Imm_11_5 at 0 range 25 .. 31;
      end record;
+   for S_Type'Bit_Order use System.Low_Order_First;
 
    type B_Type is record
       Opcode   : Opcode_Field;
@@ -94,6 +98,7 @@ package RISCV.Decoder is
        Imm_10_5 at 0 range 25 .. 30;
        Imm_12 at 0 range 31 .. 31;
      end record;
+   for B_Type'Bit_Order use System.Low_Order_First;
 
    type U_Type is record
       Opcode    : Opcode_Field;
@@ -107,6 +112,7 @@ package RISCV.Decoder is
        R_D at 0 range 7 .. 11;
        Imm_31_12 at 0 range 12 .. 31;
      end record;
+   for U_Type'Bit_Order use System.Low_Order_First;
 
    type J_Type is record
       Opcode    : Opcode_Field;
@@ -126,6 +132,7 @@ package RISCV.Decoder is
        Imm_10_1 at 0 range 21 .. 30;
        Imm_20 at 0 range 31 .. 31;
      end record;
+   for J_Type'Bit_Order use System.Low_Order_First;
 
    type Instruction_Encoding is (Word, R, I, S, B, U, J);
    type Instruction_View (Encoding : Instruction_Encoding := Word) is record
